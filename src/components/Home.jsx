cf240edf5a950284eff44bc971c1363368231697
Dragesia <brkgzlsy@gmail.com>
2023-05-31 11:08:45 +0300

    Fix typo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
             <div className="home-left"></div>
             <div className="home-right">
                 <h1>The Clone Wars Season 7 Posters</h1>
-                <p>Now available for preoder, shipping August, 2023.</p>
+                <p>Now available for preorder, shipping August, 2023.</p>
                     <button><Link to='/catalog'>SHOP NOW</Link></button>
             </div>
         </div>   
